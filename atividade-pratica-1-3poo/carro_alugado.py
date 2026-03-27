cd87cab2f2f119ce747acbf2b2002a3d18b38514
Seabrinhagemaplys <66424663+Seabrinhagemaplys@users.noreply.github.com>
2026-03-26 22:19:47 -0300

    Update carro_alugado.py

--- atividade-pratica-1-3poo/carro_alugado.py
+++ atividade-pratica-1-3poo/carro_alugado.py
@@ -1,17 +1,21 @@
 class Carro_Alugado:
-    def __init__(self, modelo: str = '', quantidade_dias: int =  0, kilometros_rodados: float = 0):
-        self.modelo = modelo
+    def __init__(self, modelo: str, quantidade_dias: int, kilometros_rodados: float):
+        self.modelo = '' 
+        self.quantidade_dias = 0
+        self.kilometros_rodados = 0
         
-        if quantidade_dias < 0:
-            self.kilometros_rodados = 0
+        if quantidade_dias < 0 or not isinstance(quantidade_dias, int):
             return
-        if kilometros_rodados
-        self.quantidade_dias = quantidade_dias
+        if kilometros_rodados < 0 or not isinstance(kilometros_rodados, int):
+            return
+
+
         self.kilometros_rodados =  kilometros_rodados
+        self.quantidade_dias =quantidade_dias
+        self.modelo = modelo
         
 
     def forneca_valor_aluguel(self):
         valor_kilometro: float = self.kilometros_rodados * 0.8
         valor_dia: float = self.quantidade_dias * 120
         return valor_kilometro + valor_dia
-
